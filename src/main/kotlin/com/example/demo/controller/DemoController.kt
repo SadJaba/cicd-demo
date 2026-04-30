f684978b3795836dc4b58536d02d4ef4eb274dd4
SadJaba <UseLessLog1@gmail.com>
2026-04-30 05:07:31 +0300

    тест ошибки на пайплайне

--- src/main/kotlin/com/example/demo/controller/DemoController.kt
+++ src/main/kotlin/com/example/demo/controller/DemoController.kt
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class DemoController {
     @GetMapping("/hello")
-    fun hello() = "Hello world!"
+    fun hello() = "Hello world"
 }

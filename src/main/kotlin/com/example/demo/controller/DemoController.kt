8f6edcfb813a53518b5f81d03f34d80851ab2dac
SadJaba <UseLessLog1@gmail.com>
2026-04-30 09:47:05 +0300

    коммит 2

--- src/main/kotlin/com/example/demo/controller/DemoController.kt
+++ src/main/kotlin/com/example/demo/controller/DemoController.kt
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class DemoController {
     @GetMapping("/hello")
-    fun hello() = "Hello!!!"
+    fun hello() = "Hello World"
 }
